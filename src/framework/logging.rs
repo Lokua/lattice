6f7b898b2b8185b1bef60c9dbad06fcc08801fa8
Lokua <dev@lokua.net>
2024-12-06 16:17:07 -0600

    feat(displacement_2): cache trig fns

--- src/framework/logging.rs
+++ src/framework/logging.rs
@@ -1,11 +1,15 @@
 use env_logger::{Builder, Env};
 use log::LevelFilter;
+// use std::io::Write;
 
 pub use log::{debug, error, info, trace, warn};
 
 pub fn init_logger() {
     Builder::from_env(Env::default().default_filter_or("lattice=info"))
         .filter_module("nannou", LevelFilter::Warn)
+        // .format(|buf, record| {
+        //     writeln!(buf, "[{}] {}", record.level(), record.args())
+        // })
         .init();
 }
 
