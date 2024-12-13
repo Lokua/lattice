c72c79d54380e5d149e0acc60ffac5ee69cfc22f
Lokua <dev@lokua.net>
2024-12-12 23:35:33 -0600

    feat(studies): more perlin tweaks

--- src/sketches/studies/perlin_loop.rs
+++ src/sketches/studies/perlin_loop.rs
@@ -1,6 +1,7 @@
 use nannou::color::*;
 use nannou::noise::NoiseFn;
 use nannou::noise::Perlin;
+use nannou::noise::Seedable;
 use nannou::prelude::*;
 
 use crate::framework::prelude::*;
@@ -16,14 +17,15 @@ pub const SKETCH_CONFIG: SketchConfig = SketchConfig {
     w: 700,
     h: 700,
     gui_w: None,
-    gui_h: Some(200),
+    gui_h: Some(260),
 };
 
 pub struct Model {
     #[allow(dead_code)]
     animation: Animation,
     controls: Controls,
-    perlin: Perlin,
+    noise: Perlin,
+    last_seed: u32,
 }
 
 impl SketchModel for Model {
@@ -34,20 +36,33 @@ impl SketchModel for Model {
 
 pub fn init_model() -> Model {
     let animation = Animation::new(SKETCH_CONFIG.bpm);
+
     let controls = Controls::new(vec![
-        Control::slider("radius", 200.0, (1.0, 500.0), 1.0),
-        Control::slider("height", 64.0, (1.0, 50.0), 0.1),
-        Control::slider("thinness", 1.5, (0.5, 5.0), 0.25),
+        Control::slider("max_rect_length", 10.0, (1.0, 200.0), 1.0),
+        Control::slider("rect_width", 1.5, (0.5, 5.0), 0.25),
+        Control::slider("noise_scale", 3.0, (0.5, 10.0), 0.1),
+        Control::slider("seed", 3.0, (3.0, 33_333.0), 33.0),
+        Control::slider("angle_resolution", 45.0, (15.0, 180.0), 1.0),
     ]);
+
+    let noise = Perlin::new();
+    let last_seed = noise.seed();
 
     Model {
         animation,
         controls,
-        perlin: Perlin::new(),
+        noise,
+        last_seed,
     }
 }
 
-pub fn update(_app: &App, _model: &mut Model, _update: Update) {}
+pub fn update(_app: &App, model: &mut Model, _update: Update) {
+    let seed = model.controls.float("seed") as u32;
+    if seed != model.last_seed {
+        model.noise = model.noise.set_seed(seed);
+        model.last_seed = seed;
+    }
+}
 
 pub fn view(app: &App, model: &Model, frame: Frame) {
     let _window_rect = app
@@ -58,23 +73,33 @@ pub fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().hsl(0.0, 0.0, 0.03);
 
-    let space = PI / 45.0;
-    let radius = model.controls.float("radius");
-    let height = model.controls.float("height");
-    let thinness = model.controls.float("thinness");
+    let circle_radius = model.controls.float("circle_radius");
+    let max_rect_length = model.controls.float("max_rect_length");
+    let rect_width = model.controls.float("rect_width");
+    let noise_scale = model.controls.float("noise_scale");
+    let angle_resolution = model.controls.float("angle_resolution");
 
-    let steps = (360.0 / space) as i32;
-    for i in 0..steps {
-        let angle = deg_to_rad(i as f32);
-        let x_off = map_range(angle.cos(), -1.0, 1.0, 0.0, 3.0);
-        let y_off = map_range(angle.sin(), -1.0, 1.0, 0.0, 3.0);
-        let n = model.perlin.get([x_off, y_off]);
-        let h = map_range(n, 0.0, 0.1, 0.0, height);
-        draw.rect()
+    let angle_increment = PI / angle_resolution;
+    let draw_rotated =
+        draw.rotate(model.animation.loop_progress(32.0) * PI * 2.0);
+
+    let total_segments = (360.0 / angle_increment) as i32;
+    for i in 0..total_segments {
+        let current_angle = i as f32 * angle_increment;
+        let noise_x = (current_angle.cos() + 1.0) * noise_scale;
+        let noise_y = (current_angle.sin() + 1.0) * noise_scale;
+        let noise_value =
+            model.noise.get([noise_x as f64, noise_y as f64]) as f32;
+        let rect_length = (noise_value + 1.0) * (max_rect_length / 2.0);
+        draw_rotated
+            .rect()
             .color(BEIGE)
-            .x_y(radius * angle.cos(), radius * angle.sin())
-            .w_h(h, thinness)
-            .rotate(angle);
+            .x_y(
+                circle_radius * current_angle.cos(),
+                circle_radius * current_angle.sin(),
+            )
+            .w_h(rect_length, rect_width)
+            .rotate(current_angle);
     }
 
     draw.to_frame(app, &frame).unwrap();
