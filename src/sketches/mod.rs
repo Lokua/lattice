4b2bbdd80360a80627c7d96c146f0a5864b1d988
Lokua <dev@lokua.net>
2024-12-13 18:50:14 -0600

    chore: cleanup

--- src/sketches/mod.rs
+++ src/sketches/mod.rs
@@ -1,13 +1,13 @@
-pub mod audio_test;
 pub mod displacement_1;
 pub mod displacement_2;
 pub mod displacement_2a;
 pub mod drop;
 pub mod drop_walk;
-pub mod midi_test;
 pub mod template;
 pub mod untitled;
 
 pub mod studies;
+pub use self::studies::audio_test;
+pub use self::studies::midi_test;
 pub use self::studies::noise;
 pub use self::studies::perlin_loop;
