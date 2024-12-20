9900335d9a13a362106fc516f629df625ff48795
Lokua <dev@lokua.net>
2024-12-20 06:15:07 -0600

    feat(d1banim): more grain params

--- src/framework/distance.rs
+++ src/framework/distance.rs
@@ -112,33 +112,6 @@ pub fn cellular(x1: f32, y1: f32, x2: f32, y2: f32, scale: f32) -> f32 {
     ((distance % scale) - scale / 2.0).abs() * 2.0
 }
 
-/// Creates a pattern similar to wood grain
-/// grain_size recommended range: [15.0, 25.0]
-/// angle_mult recommended range: [10.0, 50.0]
-// pub fn wood_grain(
-//     x1: f32,
-//     y1: f32,
-//     x2: f32,
-//     y2: f32,
-//     grain_size: f32,
-//     angle_mult: f32,
-// ) -> f32 {
-//     let distance = ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt();
-//     let angle = (y2 - y1).atan2(x2 - x1);
-//     (distance / grain_size).sin() + (angle * 5.0).sin() * angle_mult
-// }
-// pub fn wood_grain(
-//     x1: f32,
-//     y1: f32,
-//     x2: f32,
-//     y2: f32,
-//     grain_size: f32,
-//     angle_mult: f32,
-// ) -> f32 {
-//     let distance = ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt();
-//     let angle = (y2 - y1).atan2(x2 - x1);
-//     (distance / grain_size).sin() * distance + (angle * 5.0).sin() * angle_mult
-// }
 pub fn wood_grain(
     x1: f32,
     y1: f32,
@@ -151,6 +124,25 @@ pub fn wood_grain(
     let angle = (y2 - y1).atan2(x2 - x1);
     (distance / grain_size).sin() * distance * 0.5
         + (angle * 5.0).sin() * angle_mult
+}
+
+pub fn wood_grain_advanced(
+    x1: f32,
+    y1: f32,
+    x2: f32,
+    y2: f32,
+    grain_size: f32,
+    angle_mult: f32,
+    distance_power: f32,    // 2
+    distance_strength: f32, // 0.5
+    angle_frequency: f32,   // 5.0
+) -> f32 {
+    let distance = ((x2 - x1).powf(distance_power)
+        + (y2 - y1).powf(distance_power))
+    .sqrt();
+    let angle = (y2 - y1).atan2(x2 - x1);
+    (distance / grain_size).sin() * distance * distance_strength
+        + (angle * angle_frequency).sin() * angle_mult
 }
 
 /// Creates patterns similar to topographic maps
