4b4765978a0d2553a9947d735869b4e71c221b7f
Lokua <dev@lokua.net>
2025-01-08 15:08:39 -0600

    feat: fin gen2

--- src/sketches/genuary_2.rs
+++ src/sketches/genuary_2.rs
@@ -3,6 +3,8 @@ use nannou::prelude::*;
 
 use crate::framework::prelude::*;
 
+// b/w 2025 DD Running Free
+
 pub const SKETCH_CONFIG: SketchConfig = SketchConfig {
     name: "genuary_2",
     display_name: "Genuary 2: Layers Upon Layers",
@@ -46,13 +48,15 @@ struct ShaderParams {
 pub fn init_model(app: &App, wr: WindowRect) -> Model {
     let animation = Animation::new(SKETCH_CONFIG.bpm);
 
+    let disable = |_controls: &Controls| true;
+
     let controls = Controls::with_previous(vec![
         Control::slider_norm("smooth_mix", 0.5),
         Control::slider("contrast", 1.5, (0.1, 5.0), 0.1),
         Control::Separator {},
-        Control::slider("g1", 2.0, (2.0, 32.0), 1.0),
-        Control::slider("g2", 4.0, (2.0, 32.0), 1.0),
-        Control::slider("g3", 8.0, (2.0, 32.0), 1.0),
+        Control::slider_x("g1", 2.0, (2.0, 32.0), 1.0, disable),
+        Control::slider_x("g2", 4.0, (2.0, 32.0), 1.0, disable),
+        Control::slider_x("g3", 8.0, (2.0, 32.0), 1.0, disable),
         Control::Separator {},
         Control::slider_norm("post_mix", 0.5),
     ]);
@@ -77,7 +81,7 @@ pub fn init_model(app: &App, wr: WindowRect) -> Model {
 }
 
 pub fn update(app: &App, m: &mut Model, _update: Update) {
-    let time = 4.0;
+    let time = 8.0;
     let kfs = [kfr((0.0, 1.0), time)];
 
     let params = ShaderParams {
@@ -91,19 +95,41 @@ pub fn update(app: &App, m: &mut Model, _update: Update) {
         b: [
             m.animation.r_ramp(&kfs, 0.0, time * 0.5, ease_in_out),
             m.animation.r_ramp(&kfs, 0.5, time * 0.5, ease_in_out),
-            m.animation.r_ramp(&kfs, 1.0, time * 0.5, ease_in_out),
+            m.animation.r_ramp(&kfs, 0.1, time * 0.5, ease_in_out),
             m.controls.float("post_mix"),
         ],
         c: [
             m.animation.r_ramp(&kfs, 0.0, time * 0.5, ease_in_out),
             m.animation.r_ramp(&kfs, 0.5, time * 0.5, ease_in_out),
-            m.animation.r_ramp(&kfs, 1.0, time * 0.5, ease_in_out),
+            m.animation.r_ramp(&kfs, 0.1, time * 0.5, ease_in_out),
             0.0,
         ],
         d: [
-            m.controls.float("g1"),
-            m.controls.float("g2"),
-            m.controls.float("g3"),
+            // m.controls.float("g1"),
+            // m.controls.float("g2"),
+            // m.controls.float("g3"),
+            // ---
+            2.0,
+            m.animation.lrp(
+                &[
+                    (2.0, 4.0),  // stay
+                    (2.0, 4.0),  // transition
+                    (3.0, 24.0), // stay
+                    (3.0, 6.0),  // transition
+                ],
+                0.0,
+            ),
+            m.animation.lrp(
+                &[
+                    (2.0, 4.0), // stay
+                    (2.0, 4.0), // transition
+                    (4.0, 8.0), // stay
+                    (4.0, 8.0), // transition
+                    (8.0, 8.0), // stay
+                    (8.0, 6.0), // transition
+                ],
+                0.0,
+            ),
             0.0,
         ],
     };
